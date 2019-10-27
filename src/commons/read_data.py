564d489ca86b16e0cf6a3d975f0534caeded7e3e
hehaoming <hehaoming110@qq.com>
2019-10-27 22:50:41 +0800

    init project

---
+++ src/commons/read_data.py
@@ -0,0 +1,39 @@
+"""读取数据集数据
+author: hehaoming
+time: 2019/10/27 21:00
+"""
+import os
+import pandas as pd
+import numpy as np
+import logging
+
+logging.basicConfig(format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S", level=logging.DEBUG)
+
+pd.set_option('display.float_format', lambda x: '%.2f' % x)  # 为了直观的显示数字，不采用科学计数法
+pd.options.display.max_rows = 200  # 最多显示200行
+
+DATA_PATH = "../../resource/data/ex4Data"
+X_PATH = "ex4x.dat"
+Y_PATH = "ex4y.dat"
+
+
+def read_data_from_resource():
+    # 读取训练数据
+    train_data = []
+    for index, line in enumerate(open(os.path.join(DATA_PATH, X_PATH), 'r')):
+        item = line.split(" ")
+        logging.debug("%d %s %s", index, float(item[3]), float(item[6]))
+        train_data.append([float(item[3]), float(item[6])])
+    logging.debug("%s %s", "train_data", train_data)
+
+    # 读取训练标签
+    train_labels = []
+    for index, line in enumerate(open(os.path.join(DATA_PATH, Y_PATH), 'r')):
+        item = line.split(" ")
+        logging.debug("%s", item[3])
+        train_labels.append(int(float(item[3])))
+    logging.debug("%s %s", "train_labels", train_labels)
+    return np.array(train_data, dtype=np.float), np.array(train_labels, dtype=np.int)
+
+
+logging.debug("%s %s", *read_data_from_resource())
