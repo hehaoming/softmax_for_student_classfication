2ed5d3587b203deae86ad62a12da0b67e946c59f
凡 <3061259219@qq.com>
2019-11-04 14:35:43 +0800

    frames=int(iterator-1)

--- src/main/commons/show_results.py
+++ src/main/commons/show_results.py
@@ -181,7 +181,7 @@ def show_softmax_binary_result(train_data, train_labels, error_list, theta_list,
         return x_data, y_data
 
     ani = animation.FuncAnimation(
-        fig, animate, frames=iterator - 1, init_func=init, interval=0.1, blit=False, repeat=False)
+        fig, animate, frames=int(iterator - 1), init_func=init, interval=0.1, blit=False, repeat=False)
     plt.show()
 
 
